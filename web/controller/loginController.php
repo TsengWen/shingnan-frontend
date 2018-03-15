e17a43ee31f809c2cb58d4d6cffe7f046b0c0316
Unknown <u0251074@nkfust.edu.tw>
2018-03-15 16:22:57 +0800

    login: Finish password verification

--- web/controller/loginController.php
+++ web/controller/loginController.php
@@ -6,9 +6,20 @@ require_once HOME_DIR . 'model/login.lib.php';
 $login = new Login();
 
 $method = $_SERVER['REQUEST_METHOD'];
-
 switch ($method) {
     case 'POST':
+        $action = isset($_POST['action']) ? $_POST['action'] : '';
+        switch ($action) {
+            case 'login':
+                if ($login->isValidInput($_POST)) {
+                    $login->login($_POST);
+                } else {
+                    $login->view();
+                }
+                break;
+            default:
+                break;
+        }
         break;
     case 'GET':
         $action = isset($_GET['action']) ? $_GET['action'] : 'view';
@@ -17,6 +28,7 @@ switch ($method) {
                 $login->view();
                 break;
             default:
+                break;
         }
         break;
 }
--- web/model/login.lib.php
+++ web/model/login.lib.php
@@ -16,7 +16,8 @@ class Login
     /**
      * Contact constructor
      */
-    public function __construct() {
+    public function __construct()
+    {
         session_start();
         // instantiate the pdo object
         $this->db = dbSetup::getDbConn();
@@ -32,10 +33,70 @@ class Login
         }
     }
 
+    public function isValidInput($input)
+    {
+        $input['account']  = trim($input['account']);
+        $input['password'] = trim($input['password']);
+        // 測試是否為空值
+        if (strlen($input['account']) == 0) {
+            $this->error = '帳號不得為空值';
+            return false;
+        }
+        if (strlen($input['password']) == 0) {
+            $this->error = '密碼不得為空值';
+            return false;
+        }
+        $this->error = '';
+        return true;
+    }
+
     /**
-     * 顯示連絡我們
+     * 登入動作 
      */
-    public function view() {
+    public function login($input)
+    {
+        try {
+            // use SQL get password
+            $sql = "SELECT `user`.`userId`, `user`.`account`, `user`.`password` 
+                    FROM `user` 
+                    WHERE `user`.`account` = :account;";
+            $res = $this->db->prepare($sql);
+            $res->bindParam(':account', $input['account'], PDO::PARAM_STR);
+            if(!$res->execute()){
+                $this->error = '帳號輸入錯誤';
+                $this->viewLogin();
+                return ;
+            }
+
+            $result = $res->fetch();
+
+            if (password_verify($input['password'], $result['password'])) {
+                // set the session
+                $_SESSION['isLogin'] = true;
+                $_SESSION['userId']  = $result['userId'];
+                $_SESSION['account'] = $input['account'];
+
+                // clear the error message
+                $this->error = '';
+
+                // TODO: go to member page (header location)
+                echo("hello world");
+
+            } else {
+                $this->error = '密碼輸入錯誤';
+                $this->viewLogin();
+            }
+
+        } catch (Exception $e) {
+            print 'Exception : ' . $e->getMessage();
+        }
+
+    }
+    /**
+     * 登入頁面
+     */
+    public function view()
+    {
         $this->setResultMsg();
         $this->smarty->display('login.html');
     }
@@ -43,10 +104,19 @@ class Login
     /**
      * 設定訊息
      */
-    public function setResultMsg($resultMsg = 'success', $errorMsg = '') {
+    public function setResultMsg($resultMsg = 'success', $errorMsg = '')
+    {
         $this->msg = $resultMsg;
         $this->error = $errorMsg;
         $this->smarty->assign('error', $this->error);
         $this->smarty->assign('msg', $this->msg);
+    }
+
+    public function viewLogin()
+    {
+        $_SESSION['isLogin'] = false;
+        $this->smarty->assign('error', $this->error);
+        $this->smarty->assign('homePath', APP_ROOT_DIR);
+        $this->smarty->display('login.html');
     }
 }
