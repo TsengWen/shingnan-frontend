277724fd23b4589b742a6eaceff759220dcb8b54
s8w1e2ep <ha9mv8c@gmail.com>
2018-08-06 16:24:07 +0800

    Modify the layout of glasses_info

--- web/model/buy.lib.php
+++ web/model/buy.lib.php
@@ -72,7 +72,7 @@ class Buy
             $host_url = "$host//$pieces[1]/$pieces[2]/";
             foreach($style_list as $style) {
                 $sytleId = $style['styleId'];
-                $sql = "SELECT f.`frameId`, i.`path` 
+                $sql = "SELECT f.`frameId`, f.`styleId`, i.`path` 
                         FROM `frameStyle` as f LEFT JOIN `image` as i
                         ON f.`frameId` = i.`itemId`
                         WHERE f.`styleId` = '$sytleId'
@@ -85,6 +85,7 @@ class Buy
                     foreach($frame_imgs as $img) {
                         if (!is_null($img['path'])) {
                             $img['path'] = $host_url . substr($img['path'], 3);
+                            $img['sid'] = $sytleId;
                         }
                         array_push($temp_list, $img);
                     }
@@ -107,11 +108,67 @@ class Buy
      */
     public function viewDetail($inputs)
     {
-        $style = $inputs['style'];
         $sql1 = "SELECT `storeName`, `phoneNumber`, `address` FROM `store`";
         $sql2 = "SELECT * FROM `style` WHERE `isDelete` = 0";
+        $sql3 = "SELECT `frame`.*, `image`.`path` FROM `frame`
+                 LEFT JOIN `image`
+                 ON `frame`.`frameId` = `image`.`itemId`
+                 ORDER BY `frame`.`createTime` DESC";
+        $sql4 = "SELECT `brandId`, `brandName`
+                 FROM `brand`
+                 WHERE `isDelete` = 0
+                 ORDER BY `brand`.`lastUpdateTime` DESC";
+        $sql5 = "品牌名稱";
+        
+
+        if (isset($inputs['styleId'])) {
+            $styleId = $inputs['styleId'];
+            $sql3 = "SELECT * FROM `frame` RIGHT JOIN
+                    (SELECT f.`frameId`, f.`styleId`, i.`path` 
+                    FROM `frameStyle` as f LEFT JOIN `image` as i
+                    ON f.`frameId` = i.`itemId`
+                    WHERE f.`styleId` = '$styleId'
+                    ORDER BY f.`createTime` DESC) AS a
+                    ON `frame`.`frameId` = a.`frameId`";
+            $sql5 = "SELECT `styleName` FROM `style` WHERE `styleId` = '$styleId'";
+        }
+
+        if (isset($inputs['brandId'])) {
+            $brandId = $inputs['brandId'];
+            $sql3 = "SELECT `frame`.*, `image`.`path` FROM `frame`
+                     LEFT JOIN `image`
+                     ON `frame`.`frameId` = `image`.`itemId`
+                     WHERE `frame`.`brandId` = '$brandId'
+                     ORDER BY `frame`.`createTime` DESC";
+            $sql5 = "SELECT `brandName` FROM `brand` WHERE `brandId` = '$brandId'";
+        }
+        
+        echo $sql5;
+
         $store_list = $this->getSQLResult($sql1);
         $style_list = $this->getSQLResult($sql2);
+        $frames = $this->getSQLResult($sql3);
+        $brands = $this->getSQLResult($sql4);
+
+        $title_name = $sql5;
+        if ($sql5 != "品牌名稱"){
+            $title_name = $this->getSQLResult($sql5);
+            $title_name = $title_name[0][0];
+        }
+        $this->smarty->assign('tname', $title_name);
+        
+        if (!is_null($frames)) {
+            $this->smarty->assign('frames', $frames);
+            $count = count($frames);
+            $count2 -= $count%3;
+            $this->smarty->assign('check', $count2);
+            $this->smarty->assign('fcount', $count - 1);
+            $this->smarty->assign('repeat', $count%3);
+        }
+
+        if (!is_null($brands)) {
+            $this->smarty->assign('brands', $brands);
+        }
 
         if (!is_null($style_list)) {
             $this->smarty->assign('styles', $style_list);
